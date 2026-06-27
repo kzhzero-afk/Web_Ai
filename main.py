ec932c27f43c6c87efbd3b90f8171b5bf911518e
kzhzero-afk <kzh.zero@gmail.com>
2026-06-27 19:59:20 +0630

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+import shutil
+import os
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/process")
+async def process_file(
+    file: UploadFile = File(...),
+    model: str = Form(...),
+    speed: str = Form(...)
+):
+    file_path = f"{UPLOAD_DIR}/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "status": "success",
+        "file": file.filename,
+        "model": model,
+        "speed": speed,
+        "message": "Uploaded successfully"
+    }
