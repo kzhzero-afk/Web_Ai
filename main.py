ad9a43c92160082142718607bd6203d3476ff6f7
kzhzero-afk <kzh.zero@gmail.com>
2026-06-27 20:23:08 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def home():
     return {"status": "running ok"}
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    file_path = f"{UPLOAD_DIR}/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "status": "uploaded",
+        "file": file.filename
+    }
